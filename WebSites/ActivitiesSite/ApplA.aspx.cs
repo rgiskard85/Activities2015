a5cb5d1f89b0a6cb931c0becf74c3c84026863c2
rgiskard85 <r.gisgard@gmail.com>
2015-12-14 11:46:31 +0200

    Insert a comment for test

--- WebSites/ActivitiesSite/ApplA.aspx.cs
+++ WebSites/ActivitiesSite/ApplA.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//This Application A
+
 public partial class ApplA : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
